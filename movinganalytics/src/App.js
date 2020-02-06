c0d699b904b443b15c302598e53cb37ab44b104b
Aaron Nguyen <aaronnguyen992@gmail.com>
2020-02-05 16:16:08 -0800

    added guage component, added canvas logic to component to map data

--- movinganalytics/src/App.js
+++ movinganalytics/src/App.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import useFetch from './util/useFetch';
+import Guage from './components/Guage';
 
 const App = () => {
   const res = useFetch("https://cors-anywhere.herokuapp.com/" + "https://sandbox.movinganalytics.com/test/indicators");
   
   if(res.load){
-    console.log(res.data)
     return (
       <div className="App">
         <header className="App-header"></header>
-        <p>
-
-        </p>
+        <div>
+        {
+          res.data.map(data => (
+            <Guage data={data} key={data.type}/>
+          ))
+        }
+        </div>
       </div>
     );
   } else {
